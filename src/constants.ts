597e5ab27b30bcbb5041d2565f0bfe9df93d8284
BenMiriello <benmiriello@gmail.com>
2020-07-21 23:19:43 -0400

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-const devApi = 'http://api.localhost:3001/v1';
-const prodApi = 'https://veneue.herokuapp.com';
+const devApi = 'http://api.localhost:3001/v1/';
+const prodApi = 'https://veneue.herokuapp.com/';
 export const apiRoot = process.env.NODE_ENV === 'development' ? devApi : prodApi;
